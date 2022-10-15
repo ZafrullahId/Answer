6be13ecc836fb581b28d05afa12ac2f5a10ebe48
ZafrullahId <zafrullahdaprince@gmail.com>
2022-10-15 07:37:37 +0100

    Answer

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeChallenge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public static int FindingAnumberInAnArray(List<int> arr, int original)
+        {
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if(arr.Contains(original))
+                {
+                    original *= 2;
+                }
+               
+            }
+            return original;
+        }
+    }
+}
+
